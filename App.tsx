d498e95ed636ea7d8f6c069c1961d08458cce12d
100june <toypiano@naver.com>
2023-12-15 18:05:15 +0900

    style

--- App.tsx
+++ App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native';
-import Greeting from './components/Greeting';
+import Box from './components/Box';
 
 const App = () => {
-  const name = 'JSX';
   return (
     <SafeAreaView>
-      <Greeting name={name} />
+      <Box rounded={false} />
     </SafeAreaView>
   );
 };
